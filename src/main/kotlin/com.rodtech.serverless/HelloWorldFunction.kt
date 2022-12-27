d94759db4c8c582c9d5cdae4ae8df8b29a10172f
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-12-27 14:42:09 -0300

    adjusts handle

--- src/main/kotlin/com.rodtech.serverless/HelloWorldFunction.kt
+++ src/main/kotlin/com.rodtech.serverless/HelloWorldFunction.kt
@@ -3,19 +3,18 @@ package com.rodtech.serverless
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.rodtech.serverless.response.ApiGatewayResponse
-//import org.apache.log4j.Logger
+import org.apache.logging.log4j.LogManager
 
-class HelloWorldFunction : RequestHandler<Map<String, Any>, ApiGatewayResponse>{
-
-    companion object {
-       // private val log = Logger.getAnonymousLogger() //Logger.getLogger(HelloWorldFunction::class.java)
-    }
-
-    override fun handleRequest(input: Map<String, Any>, context: Context?): ApiGatewayResponse {
-       // log.info("received: $input")
+class HelloWorldFunction:RequestHandler<Map<String, Any>, ApiGatewayResponse>{
+    override fun handleRequest(input:Map<String, Any>, context: Context):ApiGatewayResponse {
+        log.info("received: ${input.keys}")
         return ApiGatewayResponse(
             body = "Hello World"
         )
+    }
+
+    companion object {
+        private val log = LogManager.getLogger(HelloWorldFunction::class.java)
     }
 
 }
