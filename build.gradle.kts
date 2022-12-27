cde2ba6505f46ad68834c23a3e7e73f4977076be
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-12-27 14:39:20 -0300

    update build.gradle.kts add shadowJar to create full jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,53 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
-
-    // Apply the application plugin to add support for building a CLI application in Java.
+    id("maven-publish")
+    id("java")
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
     application
 }
 
+group = "com.rodtech"
+version = "dev"
+description = "Kotlin Poc Basic Lambda"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    //implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    //aws lambda
-    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
-
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+    implementation("com.amazonaws:aws-lambda-java-log4j2:1.5.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.0")
 }
+
+tasks.register("deploy", Exec::class.java) {
+    dependsOn("shadowJar")
+    commandLine("serverless","deploy")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        project.setProperty("mainClassName", "")
+        transform(Log4j2PluginsCacheFileTransformer::class.java)
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+
